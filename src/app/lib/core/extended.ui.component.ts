214d2237f5aa440464f34ed33745d41dd3252962
Sedat Yeni <syeni@next-level-integration.com>
2018-04-09 11:21:04 +0200

    clean up

--- src/app/lib/core/extended.ui.component.ts
+++
@@ -1,6 +0,0 @@
-//import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import { UIComponent} from './ui.component';
-export class ExtendedUIComponent extends UIComponent {
-    //@Input()
-	label:string = 'extended-ui-component-label';    
-}
